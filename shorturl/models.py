8b299ad576163d1ce4f86b69eda49006e17d939f
yunko006 <peyrachethomas@gmail.com>
2022-04-30 00:28:35 +0200

    Create URL model + migration

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,3 +1,11 @@
+from re import M
 from django.db import models
 
 # Create your models here.
+
+class URL(models.Model):
+    url_long = models.CharField(max_length=500)
+    url_custom = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.url_custom
