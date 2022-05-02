57619025c1444d8eb18f32dab65d1e5fb39fc7e7
yunko006 <peyrachethomas@gmail.com>
2022-05-03 00:07:08 +0200

    Update choose_url view + Create redirect_url view

--- shorturl/views.py
+++ shorturl/views.py
@@ -1,5 +1,4 @@
-import re
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 from .forms import *
@@ -22,9 +21,9 @@ def home(request):
 def choose_url(request):
     """ url_a_hash = "https://www.google.com/search?q=img&rlz=1C1VDKB_frFR982FR982&sxsrf=ALiCzsbsq5YHg4KLZuLzyFgjW59o8I7Y0A:1651263407896&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiRwO7Gi7r3AhVtzoUKHcZtCsIQ_AUoAXoECAEQAw&biw=1920&bih=937&dpr=1"
     url_de_base = "127.0.0.1:8000/"
-    url_hashed = "https://www.google.com/image" 
+    url_hashed = "https://127.0.0.1:8000/image" 
     
-    return redirect('/some/url/')"""
+    return redirect(url_hashed)"""
 
     if request.method != 'POST':
         form = ChooseURLNameForm()
@@ -33,9 +32,21 @@ def choose_url(request):
         form = ChooseURLNameForm(data=request.POST)
         if form.is_valid():
             new_url = form.save(commit=False)
+            # a rajouter : check si le nom n'est pas déja prit
             new_url.save()
-            return render(request, 'shorturl/new_url.html')
-
+            context= {"new_url": new_url}
+            return render(request, 'shorturl/new_url.html', context)
+ 
     context= {"form": form}
-
     return render(request, 'shorturl/choose_url_name.html', context)
+
+
+def hash_url(request):
+    pass
+
+
+def redirect_url(request, short):
+    # check si short_url est dans la base de donnée si oui alors on redirect vers son url long else 404
+    if get_object_or_404(URL, url_custom=short):
+        long = URL.objects.get(url_custom=short)
+        return redirect(long.url_long)
