96471115d4af97f7f7da7c10f0b8b67626b7b863
yunko006 <peyrachethomas@gmail.com>
2022-05-04 15:14:05 +0200

    Add HashURLForm in forms.py

--- shorturl/forms.py
+++ shorturl/forms.py
@@ -7,3 +7,9 @@ class ChooseURLNameForm(forms.ModelForm):
     class Meta:
         model = URL
         fields = ['url_long', 'url_custom']
+
+
+class HashURLNameForm(forms.ModelForm):
+    class Meta:
+        model = URL
+        fields = ['url_long']
