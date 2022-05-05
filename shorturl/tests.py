241a360f2e83fe8a897fe5b295bdb3f96099d075
yunko006 <peyrachethomas@gmail.com>
2022-05-05 23:35:47 +0200

    Create a test for the model

--- shorturl/tests.py
+++ shorturl/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from urllib import request
+from django.test import SimpleTestCase, TestCase
 
-# Create your tests here.
+from shorturl import views
+from .models import URL
+from django.urls import resolve, reverse
+from .forms import *
+
+
+# models test
+class URLTest(TestCase):
+
+    def create_url(self, url_long="test avec un url long", url_custom='custom name'):
+        return URL.objects.create(url_long=url_long, url_custom=url_custom)
+
+    def test_url_creation(self):
+        u = self.create_url()
+        self.assertTrue(isinstance(u, URL))
+        self.assertEqual(u.__str__(), u.url_custom)
