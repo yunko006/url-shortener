e5783170fd0e007ae6ffd2912894ee8375639d9a
yunko006 <peyrachethomas@gmail.com>
2022-07-21 15:36:43 +0200

    Altered a field in URL model, create a funct + migrations

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,10 +1,19 @@
+import secrets
+from string import ascii_uppercase, ascii_lowercase, digits
+from xmlrpc.client import TRANSPORT_ERROR
 from django.db import models
 
 # Create your models here.
 
 class URL(models.Model):
-    url_long = models.CharField(max_length=500)
+    url_long = models.CharField(max_length=500, unique=True)
     url_custom = models.CharField(max_length=20, unique=True)
+
 
     def __str__(self) -> str:
         return self.url_custom
+
+
+    def create_short_url(self, length: int = 8) -> str:
+        chars = ascii_uppercase + ascii_lowercase + digits
+        return "".join(secrets.choice(chars) for _ in range(length))
