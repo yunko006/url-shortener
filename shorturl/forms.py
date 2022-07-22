a0771ed60fcc7cd0ceb95cc414fe61eaea1f63b0
yunko006 <peyrachethomas@gmail.com>
2022-07-22 17:57:06 +0200

    Create a clean_url_custom funct to forbid certain names

--- shorturl/forms.py
+++ shorturl/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import URL
 
@@ -8,6 +9,17 @@ class ChooseURLNameForm(forms.ModelForm):
         model = URL
         fields = ['url_long', 'url_custom']
 
+    def clean_url_custom(self):
+        url_custom = self.cleaned_data['url_custom']
+        interdit = ['choose', 'retrieve', 'generate']
+        if not url_custom:
+            return url_custom
+
+        if url_custom in interdit:
+            raise ValidationError("You can't use thoses names : choose, retrieve and generate sorry!")
+            # self.add_error('url_custom', 'Hello')
+
+        return url_custom
 
 class HashURLNameForm(forms.ModelForm):
     class Meta:
